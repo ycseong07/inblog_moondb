9793d095ba56cebe298e783c82edec9f50abfa18
ycseong07 <ycseong07@gmail.com>
2023-08-27 15:03:32 +0900

    add: brew_install.sh

---
+++ brew_install.sh
@@ -0,0 +1,34 @@
+## Update Homebrew
+brew update
+
+## Chrome
+brew install --cask google-chrome
+
+## Git CLI
+brew install gh
+
+## iTerm2 + zsh
+brew install --cask iterm2
+brew install zsh
+
+## Python3
+brew install python3
+
+## IDE
+brew install --cask visual-studio-code
+brew install --cask pycharm-ce
+
+## Docker
+brew install --cask docker
+
+## Utilities for convenience
+brew install --cask spectacle
+brew install --cask karabiner-elements
+brew install --cask itsycal
+brew install httpie
+brew install flux
+brew install tree
+
+## Productivity Tools
+brew install --cask notion
+brew install --cask slack
